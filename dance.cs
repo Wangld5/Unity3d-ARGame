0103ca5f5a108b8b49551e039f8ea5e2a2629723
wangld5 <wangld4195@163.com>
2018-06-19 23:17:00 +0800

    Script

---
+++ dance.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class dance : MonoBehaviour {
+
+	DefaultTrackableEventHandler dte;	
+	public GameObject MIKU;
+	GameObject miku;
+	// Use this for initialization
+	void Awake(){
+		dte = GetComponent<DefaultTrackableEventHandler> ();
+		dte.OnTrackingFoundEvent += Dte_OnTrackingFoundEvent;
+		dte.OnTrackingLostEvent += Dte_OnTrackingLostEvent;
+	}
+
+	void Dte_OnTrackingLostEvent ()
+	{
+		List<Transform> lst = new List<Transform>();
+		foreach(Transform child in this.transform){
+			lst.Add (child);
+		}
+		for (int i = 0; i < lst.Count; i++) {
+			Destroy (lst [i].gameObject);
+		}
+	}
+
+	void Dte_OnTrackingFoundEvent ()
+	{
+		miku = Instantiate (MIKU, this.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
